a2d925f338348a0f68575a24795b536fed045007
bobbidigi <bobbidigi34@gmail.com>
2017-04-27 18:12:42 -0400

    new index

---
+++ script.js
@@ -0,0 +1,13 @@
+function openNav() {
+	document.getElementById('sidenav').style.width = "100%";
+
+	//Hides menu icon 
+	document.getElementById('opn').style.display = "none";
+}
+
+function closeNav() {
+	document.getElementById('sidenav').style.width = "0px";
+
+	//Unhides menu icon
+	document.getElementById('opn').style.display = "block";
+}
