3333ba7e416a32922308e551b54408db4e23d213
bobbidigi <bobbidigi34@gmail.com>
2017-05-13 16:42:37 -0400

    put jq in script.js

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function closeNav() {
 	//Unhides menu icon
 	document.getElementById('opn').style.display = "block";
 }
+
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+      var name = $('#inputName')
+      var email = $('#inputEmail')
+      var message = $('#inputMessage')
+    
+      if(name.val() == "" || email.val() == "" || message.val() == "") {
+        $('.submit-fail').fadeToggle(400);
+        return false;
+      }
+      else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/bobbidigi34@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeToggle(400);
+      }
+    });
+  
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
+});
