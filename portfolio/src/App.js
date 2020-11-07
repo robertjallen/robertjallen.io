6bfe96165c8e6f33b39c32b6e135e644994aad83
robertjallen <bobbidigi34@gmail.com>
2020-11-06 20:04:13 -0500

    add about component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About/>
     </div>
   );
 }
