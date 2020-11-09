2df086f9e78ab0e98904bb36f43dc2f7cea50413
robertjallen <bobbidigi34@gmail.com>
2020-11-09 14:32:47 -0500

    add skills

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -2,11 +2,49 @@ import React from 'react'
 
 import html from '../images/icons/html.jpg'
 import css from '../images/icons/css.jpg'
+import javascript from '../images/icons/js.jpg'
+import react from '../images/icons/react.jpg'
+import redux from '../images/icons/redux.png'
+import jest from '../images/icons/jest.png'
+import sass from '../images/icons/sass.jpg'
+import node from '../images/icons/nodejs.jpg'
+import sql from '../images/icons/sql.jpg'
+import git from '../images/icons/git.jpg'
+
+import responsive from '../images/icons/responsivedesign.jpg'
+import photoshop from '../images/icons/photoshop.jpg'
+import figma from '../images/icons/figma.png'
+import github from '../images/icons/github.jpg'
+import python from '../images/icons/python.png'
+
+
 import Skill from './Skill'
 
 
 export default function Skills() {
-  const skills = [{name: "HTML", image: html }, {name: "CSS", image: css}, {name: "JavaScript"}];
+  const webSkills = [
+    {name: "HTML", image: html },
+    {name: "CSS", image: css},
+    {name: "JavaScript", image: javascript},
+    {name: "REACT", image: react},
+    {name: "REDUX", image: redux}
+  ];
+
+  const tools = [
+    {name: "JEST", image: jest },
+    {name: "SASS", image: sass},
+    {name: "NODE", image: node},
+    {name: "SQL", image: sql},
+    {name: "GIT", image: git}
+  ]
+
+  const other = [
+    {name: "RESPONSIVE", image: responsive },
+    {name: "PHOTOSHOP", image: photoshop},
+    {name: "FIGMA", image: figma},
+    {name: "GITHUB", image: github},
+    {name: "PYTHON", image: python}
+  ]
 
   return (
     <div>
@@ -17,76 +55,27 @@ export default function Skills() {
           <div class="col wow bounceInUp" data-wow-duration="1.5s">
             <h3>Web Development</h3>
             <div class="skill-sec-container">
-              {skills.map(skill => {
+              {webSkills.map(skill => {
                 return <Skill skill={skill}/> 
               })}
-
-              
-              <div class="js skill">
-                <img class="skill-img" src="images/icons/js.jpg" alt="JavaScript"/>
-                <p>JavaScript</p>
-              </div>
-              <div class="js skill">
-                <img class="skill-img" src="images/icons/react.jpg" alt="react"/>
-                <p>React</p>
-              </div>
-              <div class="js skill">
-                <img class="skill-img " src="images/icons/redux-283024.png" alt="redux"/>
-                <p>Rudux</p>
-              </div>
             </div>
           </div><br/>
+
           <div class="col wow bounceInUp" data-wow-delay=".4s" data-wow-duration="1.5s">
             <h3>Tools & Technologies</h3>
             <div class="skill-sec-container">
-
-              <div class="react skill">
-                <img class="skill-img framework" src="images/icons/jest.png" alt="Jest testing library"/>
-                <p>Jest</p>
-              </div>
-              <div class="sass skill">
-                <img class="skill-img" src="images/icons/sass.jpg" alt="C++"/>
-                <p>SASS</p>
-              </div>
-              <div class="mysql skill">
-                <img class="skill-img" src="images/icons/nodejs.jpg" alt="node"/>
-                <p>node</p>
-              </div>
-              <div class="sql skill">
-                <img class="skill-img" src="images/icons/sql.jpg" alt="Structured Query Language" />
-                <p>SQL</p>
-              </div>
-              <div class="git skill">
-                <img class="skill-img" src="images/icons/git.jpg" alt="Git"/>
-                <p>Git</p>
-              </div>
+              {tools.map(skill => {
+                  return <Skill skill={skill}/> 
+                })}
             </div>
           </div>
-
 
           <div class="col wow bounceInUp" data-wow-delay=".8s" data-wow-duration="1.5s">
             <h3>Others</h3>
             <div class="skill-sec-container">
-              <div class="design skill">
-                <img class="skill-img framework" src="images/icons/responsivedesign.jpg" alt="responsivedesign"/>
-                <p>Responsive</p>
-              </div>
-              <div class="photoshop skill">
-                <img class="skill-img" src="images/icons/photoshop.jpg" alt="Adobe Photoshop"/>
-                <p>Photoshop</p>
-              </div>
-              <div class="design skill">
-                <img class="skill-img framework" src="images/icons/figma.png" alt="figma"/>
-                <p>Figma</p>
-              </div>
-              <div class="github skill">
-                <img class="skill-img" src="images/icons/github.jpg" alt="GitHub"/>
-                <p>GitHub</p>
-              </div>
-              <div class="design skill">
-                <img class="skill-img" src="images/icons/Python.png" alt="python"/>
-                <p>Python</p>
-              </div>
+              {other.map(skill => {
+                    return <Skill skill={skill}/> 
+                  })}
             </div>
           </div>
         </div>
