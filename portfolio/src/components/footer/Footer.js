12aeebaf55444aef3f22621dc6ba7af713c9f5d5
robertjallen <bobbidigi34@gmail.com>
2021-04-23 13:56:32 -0400

    update footer buttons

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './footer.css'
 
 export default function Footer() {
     return (
-        <div className="footer">
+        <footer className="footer">
             <p>Developed by <strong>Rob Allen</strong>.</p>
             <div className="social-container">
               <a className="social" href="https://github.com/robertjallen" target="_blank">github</a><br/> 
               <a className="social" href="https://www.linkedin.com/in/robertallendev/" target="_blank">linkedIn</a>
             </div>            
-        </div>
+        </footer>
     )
 }
