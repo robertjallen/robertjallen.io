f28ed3c3522f68d96eab4cd4ec09395a2227ad6b
robertjallen <bobbidigi34@gmail.com>
2021-04-23 16:50:27 -0400

    add awards and certs and titles and data

--- portfolio/src/components/awards/data.js
+++ portfolio/src/components/awards/data.js
@@ -1,9 +1,23 @@
 import equalithon from '../../images/awards/equalithon.png'
 import frontend from '../../images/awards/frontend.png' 
+import hacktoberfest from '../../images/awards/hacktoberfest.png'
+import influencer from '../../images/awards/Influencer.png'
+import mentor from '../../images/awards/mentor.png'
+import year from '../../images/awards/year-Badge.png'
+import javascript from '../../images/awards/javascript.jpg'
+import scholarship from '../../images/awards/scholarship.JPG'
+import hacktober2020 from '../../images/awards/hacktoberfest2020.png'
 
 const imageData = [
-  {image: equalithon},
-  {image: frontend},
+  {image: equalithon, title: "Equalithon"},
+  {image: frontend, title: "Udacity Frontend Nanodegree"},
+  {image: hacktoberfest, title: "Hacktoberfest"},
+  {image: influencer, title: "Esteem.com influncer"},
+  {image: mentor, title: "Essteem.com mentor"},
+  {image: year, title: "2 year badge"},
+  {image: javascript, title: "Javascript"},
+  {image: scholarship, title: "Grow With Google Scholarship"},
+  {image: hacktober2020, title: "HacktoberFest"}
 ]
 
 export default imageData
