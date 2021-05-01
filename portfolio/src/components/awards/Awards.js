8772b4a5d3ce42dc1a63082b05a3b1d1d4d4eef1
robertjallen <bobbidigi34@gmail.com>
2021-05-01 19:08:55 -0400

    add awards pagination

--- portfolio/src/components/awards/Awards.js
+++ portfolio/src/components/awards/Awards.js
@@ -1,21 +1,39 @@
 import React, {useState} from 'react'
-import awards from './data'
+import data from './data'
 import Award from './Award'
 import styles from './awards.css'
 import Animation from '../animation/Animation'
+import AwardsPagination from '../pagination/AwardsPagination'
+
 
 export default function Awards() {
 
-  const [awardImages, setAwardImages] = useState(awards)
+  const [awards, setAwards] = useState(data)
+  const [awardsPerPage, setAwardsPerPage] = useState(4)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  // Get current Awards
+  const indexOfLastAward = currentPage * awardsPerPage;
+  const indexOfFirstAward = indexOfLastAward - awardsPerPage;
+  const currentAwards = awards.slice(indexOfFirstAward, indexOfLastAward);
+
+     // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
   
   return (
     <div className="awards-container">
       <Animation/>
-      <h1 className="sections">Awards</h1>
+      <h1 id="more-awards" className="sections">Awards</h1>
       <hr className="hrs"/>
-      {awards.map(award => {
+      {currentAwards.map(award => {
         return <Award award={award}/>
       })}
+
+      <AwardsPagination
+        awardsPerPage={awardsPerPage}
+        totalAwards={awards.length}
+        paginate={paginate}
+      />      
     </div>
   )
 }
